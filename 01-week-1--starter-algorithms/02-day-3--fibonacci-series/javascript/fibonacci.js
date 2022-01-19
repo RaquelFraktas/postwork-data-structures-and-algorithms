e4dfffb77821f8886f2f24f50c8be2a9f4e5273d
Raquel Fraktas <72051783+RaquelFraktas@users.noreply.github.com>
2022-01-18 20:39:52 -0500

    fibonacci solution

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
-  // type your code here
+  if (num < 2){
+    return num
+  } else {
+    let inputAry= [0,1]
+    for(let i=1; i < num; i++){
+      let fibForm = inputAry[i] + inputAry[i-1]
+      inputAry.push(fibForm)
+    } 
+    return inputAry[num]
+  }
 }
 
 if (require.main === module) {
@@ -22,3 +31,9 @@ module.exports = fibonacci;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+// return an output in the fibonacci sequence based off the indexed number (input)
+// the third number in a fib seq, is the sum of the 2 preceeding numbers
+// create an array to hold the values from input
+// create a formula that adds new values into the fib sequence and adds it to the array variable
+// iterate that formula by the times from the input
+// grab the number at the given index from the created array
