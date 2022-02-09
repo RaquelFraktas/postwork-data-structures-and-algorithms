7ceab1a3ad45f43b750ae6e5ec284abadbdb38de
Raquel Fraktas <72051783+RaquelFraktas@users.noreply.github.com>
2022-02-08 21:23:45 -0500

    add solution

--- 01-week-1--starter-algorithms/04-day-5--find-shortest-string/ruby/find_shortest_string.rb
+++ 01-week-1--starter-algorithms/04-day-5--find-shortest-string/ruby/find_shortest_string.rb
@@ -1,5 +1,11 @@
 def find_shortest_string(arr)
-  # type your code in here
+  shorter = arr[0]
+  arr.each do |string|
+    if string.length < shorter.length
+      shorter = string
+    end
+  end
+  shorter
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -23,3 +29,15 @@ end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+
+# // Given an Array of strings, return the shortest string. If there is more than one string of that length, return the string that comes first in the list. The Array will have a minimum length of 1.
+
+#set a variable equal to the first element in the array
+# loop through the array
+# check to see if that an element in that iteration is shorter than the iterated variable
+# if yes, rest the declaration of that variable
+# then return that variable declaration
+
+# this works bc we are setting the first element to a variable
+# when we iterate, we check to see if theres anything shorter than the set variable
+# we return that variable after iteration, if there is no other matches, then the original set variable is spat out
