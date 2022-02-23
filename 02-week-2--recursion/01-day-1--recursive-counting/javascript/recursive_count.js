fbc6558fd1316f9ffa7f0ccdc7b606db129e0b0a
Raquel Fraktas <72051783+RaquelFraktas@users.noreply.github.com>
2022-02-22 20:59:41 -0500

    add solution

--- 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
+++ 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
@@ -1,5 +1,9 @@
 function recursiveCount(num = 0) {
-  // type your code here
+  if (num >= 9){
+    return console.log(num)
+  }
+  console.log(num)
+  recursiveCount(num + 1)
 }
 
 if (require.main === module) {
@@ -14,3 +18,7 @@ module.exports = recursiveCount;
 //recursively call the function
 // increase the counter each time the method is called
 //stop recursion if it hits 9. counter starts at 0
+
+//this works because i first had to write the conditional for the base case
+// then return the function if the base case was hit
+// if its not hit, then console log the number, and recursively call the number with an added 1
